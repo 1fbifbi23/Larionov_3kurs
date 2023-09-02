8c621f513f4ff19495503f1eb456cb625a7240fd
Даниил Ларионов <danii.llarionov111@gmail.com>
2023-09-02 13:11:16 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/") 
 @app.route("/index")
 def start():
-    return """
-<!doctype html>
-<html>
-    <head>
-        <title>Ларионов Даниил Сергеевич, Лаб1</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
-
-        <h1><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></h1>
-
-        <footer>
-            &copy; Даниил Ларионов, ФБИ-12, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-"""
+    return redirect("/menu", code=302)
 @app.route("/lab1")
 def lab1():
         return """
@@ -41,6 +23,27 @@ def lab1():
 Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
 называемых микрофреймворков — минималистичных каркасов
 веб-приложений, сознательно предоставляющих лишь самые базовые возможности</h1>
+
+        <footer>
+            &copy; Даниил Ларионов, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+@app.route("/menu")
+def menu():
+      return """
+<!doctype html>
+<html>
+    <head>
+        <title>Ларионов Даниил Сергеевич, Лаб1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <h1><a href="http://127.0.0.1:5000/lab1">Первая лабораторная</a></h1>
 
         <footer>
             &copy; Даниил Ларионов, ФБИ-12, 3 курс, 2023
